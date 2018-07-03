66fa89ce50eb5a2816429d9c08dcae337a4bcdd7
joel-ace <joelakwes@gmail.com>
2018-07-03 15:40:39 +0100

    setu database to use postgres and create user migration

---
+++ database/migrations/1503250034279_user.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const Schema = use('Schema')
+
+class UserSchema extends Schema {
+  up () {
+    this.create('users', (table) => {
+      table.increments()
+      table.string('username', 80).notNullable().unique()
+      table.string('full_name', 254).notNullable()
+      table.string('email', 254).notNullable().unique()
+      table.string('password', 60).notNullable()
+      table.string('strategy', 60).notNullable().defaultTo('local')
+      table.string('account_type', 255).defaultTo('user')
+      table.timestamps()
+    })
+  }
+
+  down () {
+    this.drop('users')
+  }
+}
+
+module.exports = UserSchema
